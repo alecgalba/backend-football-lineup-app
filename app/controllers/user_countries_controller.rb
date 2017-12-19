6bb448ea08622dab7946cde7d0a0a24c5a19b91e
alecgalba <alecgalba@gmail.com>
2017-12-18 21:04:39 -0500

    set up preliminary routes, also added index to user countries cuz forgot

--- app/controllers/user_countries_controller.rb
+++ app/controllers/user_countries_controller.rb
@@ -1,5 +1,10 @@
 class UserCountriesController < ApplicationController
 
+  def index
+    user = User.find(params[:id])
+    user_countries = user.user_countries
+    render json: user_countries
+  end
 
   def destroy
     user_country = UserCountry.find_by(user_id: params[:id], country_id: params[:country_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post '/search',     to: 'countries#search'
+  get '/:id/countries',   to: 'countries#index'
+  post '/countries',      to: 'countries#create'
+  delete '/countries',    to: 'countries#destroy'
+
+  get '/:id/competitions',  to: 'competitions#index'
+  post '/competitions',     to: 'competitions#create'
+  delete '/competitions',   to: 'competitions#destroy'
+
+  get '/:id/events',  to: 'events#index'
+  post '/events',     to: 'events#create'
+  delete '/events',   to: 'events#destroy'
+
+  post '/login',  to: 'sessions#create'
+  post '/signup', to: 'users#create'
+
+  get '/:id/user_countries', to: 'user_countries#index'
+  delete '/:id/user_countries', to: 'user_countries#destroy'
+
+  get '/:id/user_competitions', to: 'user_competitions#index'
+  delete '/:id/user_competitions', to: 'user_competitions#destroy'
+
+  get '/:id/user_events', to: 'user_events#index'
+  delete '/:id/user_events', to: 'user_events#destroy'
+
 end
